2388a4cf4c7a0892f835eaf302a0151520ab655f
jbwharris <james@slideaway.ca>
2024-08-30 21:56:05 -0400

    Add WMFO

---
+++ js/stations/wmfo.js
@@ -0,0 +1,13 @@
+const stationData = {
+  wmfo: {
+    stationName: "WMFO",
+    albumArt: "iImg",
+    location: "Medford, MS 🇺🇸",
+    webUrl: "https://www.wmfo.org/",
+    streamUrl: "http://wmfo-duke.orgs.tufts.edu:8000/",
+    apiUrl: "https://scraper2.onlineradiobox.com/us.wmfo?l=0",
+    timestamp: "updated",
+    orbPath: true,
+    filter: ["WMFO"],
+  }
+};
