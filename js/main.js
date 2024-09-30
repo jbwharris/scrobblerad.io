489121c5c254de0538c83e44e24d1856d07f023e
jbwharris <james@slideaway.ca>
2024-09-29 23:44:55 -0400

    Fix cache header bug

--- js/main.js
+++ js/main.js
@@ -705,13 +705,9 @@ class RadioPlayer {
                 let stationUrl = this.addCacheBuster(this.currentStationData[this.stationName].apiUrl);
 
                 const fetchOptions = {
-                  method: this.currentStationData[this.stationName].method || 'GET',
-                  headers: {
-                    ...this.currentStationData[this.stationName].headers,
-                    'X-Exclude-From-Cache': 'true' // Custom header to signal the service worker
-                  },
+                    method: this.currentStationData[this.stationName].method || 'GET',
+                    headers: this.currentStationData[this.stationName].headers || {},
                 };
-
 
                 fetch(stationUrl, fetchOptions)
                     .then((response) => {
