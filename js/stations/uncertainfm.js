3b450b645ed782f2b0d5640e28e933503b440b76
jbwharris <james@slideaway.ca>
2024-11-12 22:10:27 -0500

    Add filter

--- js/stations/uncertainfm.js
+++ js/stations/uncertainfm.js
@@ -10,6 +10,6 @@ const stationData = {
     apiUrl: "https://api.live365.com/station/a23389",
     timestamp: "current-track.start",
     timezone: "America/New_York",
-    filter: ["UncertainFM"],
+    filter: ["Uncertain", "Take Care of Yourselves Please", "Cover Lover"],
   }
 };
