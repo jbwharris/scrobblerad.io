ef59a31c69c3f2538ba6746d8e0c4114d44cb6b6
jbwharris <james@slideaway.ca>
2024-11-17 21:18:18 -0500

    Update streaming URL

--- js/stations/indie88.js
+++ js/stations/indie88.js
@@ -4,7 +4,7 @@ const stationData = {
     albumArt: "iImg",
     location: "Toronto, ON 🇨🇦",
     webUrl: "https://indie88.com",
-    streamUrl: "https://cob-ais.leanstream.co/CINDFM-MP3",
+    streamUrl: "https://localradio.streamb.live/SB00348",
     apiUrl: "https://scraper2.onlineradiobox.com/ca.cind?l=0",
     timestamp: "updated",
     timezone: "America/Toronto",
