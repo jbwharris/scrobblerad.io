0afcaa80f7b61e8bcbf9789093bbe9c44adde15b
jbwharris <james@slideaway.ca>
2025-02-09 16:14:12 -0500

    Update The Bridge API source

--- js/stations/thebridge.js
+++ js/stations/thebridge.js
@@ -1,14 +1,15 @@
 const stationData = {
   thebridge: {
     stationName: "The Bridge 90.9",
-    albumArt: "iImg",
+    artist: "data.0.artist",
+    song: "data.0.title",
+    album: "data.0.album",
     location: "Kansas City, MO 🇺🇸",
     webUrl: "https://bridge909.org/",
     streamUrl: "https://live.amperwave.net/direct/publictv19-ktbgfmaac-imc1",
-    apiUrl: "https://scraper2.onlineradiobox.com/us.ktbg?l=0",
-    timestamp: "updated",
+    apiUrl: "https://ca.api.iheart.com/api/v3/live-meta/stream/7200/trackHistory?limit=1",
+    timestamp: "data.0.endTime",
     timezone: "America/Chicago",
-    orbPath: true,
     filter: ["The Bridge", "Sonic Spectrum"],
   }
 };
