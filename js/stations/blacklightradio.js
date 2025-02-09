c5bfb59a94639f7ecd3cd19609f42276190b429d
jbwharris <james@slideaway.ca>
2025-02-09 16:15:11 -0500

    Improve album art sourcing

--- js/stations/blacklightradio.js
+++ js/stations/blacklightradio.js
@@ -3,7 +3,6 @@ const stationData = {
     stationName: "Blacklight Radio",
     artist: "current-track.artist",
     song: "current-track.title",
-    albumArt: "current-track.art",
     location: "Tulsa, OK 🇺🇸",
     webUrl: "https://blacklightradio.com/",
     streamUrl: "https://streaming.live365.com/a62286_2",
