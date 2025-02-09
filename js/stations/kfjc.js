35872abfcb4d1c7601244d08f5bc4db4a5b624cd
jbwharris <james@slideaway.ca>
2025-02-09 16:13:46 -0500

    Add KFJC

---
+++ js/stations/kfjc.js
@@ -0,0 +1,16 @@
+const stationData = {
+  kfjc: {
+    stationName: "KFJC",
+    artist: "playlist.0.artist",
+    song: "playlist.0.track_title",  
+    album: "playlist.0.album_title",
+    location: "Los Altos Hills, CA 🇺🇸",
+    webUrl: "https://kfjc.org",
+    streamUrl: "http://netcast.kfjc.org/kfjc-320k-aac",
+    apiUrl: "https://kfjc.org/api/playlists/index.php",
+    timestamp: "playlist.0.time_played",
+    timezone: "UTC",
+    phpString: true,
+    reverseArray: true,
+  }
+};
