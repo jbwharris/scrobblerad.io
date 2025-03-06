a5f343b5a3f60802449edbe17e686987c9da18eb
jbwharris <james@slideaway.ca>
2025-03-06 16:34:37 -0500

    Fix URL

--- js/stations/indieblend.js
+++ js/stations/indieblend.js
@@ -5,7 +5,7 @@ const stationData = {
     song: "current-track.title",
     albumArt: "current-track.art",
     location: "Washington, DC 🇺🇸",
-    webUrl: "https://indie312.com/",
+    webUrl: "https://live365.com/station/The-Indie-Blend-a51326",
     streamUrl: "https://das-edge13-live365-dal02.cdnstream.com/a51326",
     apiUrl: "https://api.live365.com/station/a51326",
     timestamp: "current-track.start",
