dd4137586f67dfe703c81ef21c084d4dc2915a68
jbwharris <james@slideaway.ca>
2025-03-06 16:34:45 -0500

    Add filters

--- js/stations/krsh.js
+++ js/stations/krsh.js
@@ -8,5 +8,7 @@ const stationData = {
     webUrl: "https://www.krsh.com",
     streamUrl: "https://playerservices.streamtheworld.com/api/livestream-redirect/KRSHFMAAC.aac",
     apiUrl: "https://api.itmwpb.com/nowplaying/v3/1697/KRSHFM",
+    timezone: "America/Los_Angeles",
+    filter: ["Station ID", "Progressive", "- CM", "BELLAROSA COFFEE", "PFIZER", "ZIPRECRUITER", "NEWSMAX"],
   }
 };
