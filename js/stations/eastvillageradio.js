742e961b96d20ae6531a7072a127f9d86c551035
jbwharris <james@slideaway.ca>
2025-03-06 16:34:55 -0500

    Add station

---
+++ js/stations/eastvillageradio.js
@@ -0,0 +1,14 @@
+const stationData = {
+  eastvillageradio: {
+    stationName: "East Village Radio",
+    location: "New York, NY 🇺🇸",
+    webUrl: "https://eastvillageradio.com",
+    streamUrl: "https://east-village-radio.radiocult.fm/stream",
+    apiUrl: "https://eastvillageradio.com/player-text-gw/",
+    timezone: "America/New_York",
+    phpString: true,
+    htmlString: true,
+    stringPath: true,
+    flipMeta: true
+  }
+};
