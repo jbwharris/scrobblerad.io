9634371510df4eebce63300bddf7fc9b45036f33
jbwharris <james@slideaway.ca>
2025-03-14 22:47:55 -0400

    Freeform Portland

---
+++ js/stations/kffp.js
@@ -0,0 +1,12 @@
+const stationData = {
+  kffp: {
+    stationName: "KFFP",
+    location: "Portland, OR 🇺🇸",
+    webUrl: "https://freeformportland.org/",
+    streamUrl: "http://listen.freeformportland.org:8000/stream",
+    apiUrl: "https://widgets.spinitron.com/widget/now-playing-v2?callback=_spinitron05891577247031242171657762111&station=kffp&num=0&sharing=0&player=0&cover=0&merch=0&meta=0",
+    spinPath: true,
+    timezone: "America/Los_Angeles",
+    filter: ["KFFP"],
+  }
+};
