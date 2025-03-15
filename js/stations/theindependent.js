14ffcbcb8704ff16c27e754608d12f5215278309
jbwharris <james@slideaway.ca>
2025-03-14 22:48:12 -0400

    Update image source

--- js/stations/theindependent.js
+++ js/stations/theindependent.js
@@ -2,7 +2,6 @@ const stationData = {
   theindependent: {
     stationName: "The Independent",
     location: "Jacksonville, FL 🇺🇸",
-    albumArt: "iImg",
     webUrl: "https://wjct.org/jaxmusic/the-independent/",
     streamUrl: "https://18373.live.streamtheworld.com/WJCT_HD4.mp3",
     apiUrl: "https://scraper2.onlineradiobox.com/us.wjcthd4?l=0",
