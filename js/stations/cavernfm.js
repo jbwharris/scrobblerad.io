252382785d044e2578bd962af2b601996620b83f
jbwharris <james@slideaway.ca>
2025-03-16 19:29:10 -0400

    Add KQUA

---
+++ js/stations/cavernfm.js
@@ -0,0 +1,15 @@
+const stationData = {
+  cavernfm: {
+    artist: "artist",
+    song: "title",
+    album: "album",
+    stationName: "Cavern.FM",
+    location: "Portland, OR 🇺🇸",
+    webUrl: "https://www.cavern.fm/",
+    streamUrl: "https://stream.radiojar.com/bt6e693868tvv.m4a?1742066166",
+    apiUrl:
+      "https://proxy.radiojar.com/api/stations/bt6e693868tvv/now_playing/?format=json",
+    timestamp: "0.last_seen",
+    timezone: "America/Los_Angeles",
+  }
+};
---
+++ js/stations/kqua.js
@@ -0,0 +1,16 @@
+const stationData = {
+  kqua: {
+    stationName: "KQUA The River",
+    artist: "current-track.artist",
+    song: "current-track.title",
+    albumArt: "current-track.art",
+    location: "Roseburg, OR 🇺🇸",
+    webUrl: "https://kqua.org",
+    streamUrl: "https://streaming.live365.com/a44781",
+    apiUrl: "https://api.live365.com/station/a44781",
+    timestamp: "current-track.start",
+    duration: "current-track.duration",
+    timezone: "America/Los_Angeles",
+    isFuture: true,
+  },
+}
