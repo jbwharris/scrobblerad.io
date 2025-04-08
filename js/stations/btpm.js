049e189c685f02af792d48ab8fa540872bf074d2
jbwharris <james@slideaway.ca>
2025-04-07 22:55:17 -0400

    WBFO to BTPM

--- js/stations/btpm.js
+++ js/stations/btpm.js
@@ -1,10 +1,11 @@
 const stationData = {
-  wbfo: {
-    stationName: "WBFO - The Bridge",
-    webUrl: "https://www.wbfo.org/the-bridge",
+  btpm: {
+    stationName: "BTPM - The Bridge",
+    webUrl: "https://btpm.org/the-bridge",
+    location: "Buffalo, NY 🇺🇸",
     streamUrl: "https://17633.live.streamtheworld.com/WBFO_THE_BRIDGEAAC.aac",
     apiUrl: "https://scraper2.onlineradiobox.com/us.thebridge?l=0",
-    filter: ["WBFO", "The Bridge"],
+    filter: ["WBFO", "BTPM", "The Bridge"],
     timestamp: "updated",
     timezone: "America/New_York",
     orbPath: true,
