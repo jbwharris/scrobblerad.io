aa29a17605c8ed09c644ace7bbb8b651f14b7d7c
jbwharris <james@slideaway.ca>
2025-04-07 22:55:44 -0400

    Add KXLL

---
+++ js/stations/kxll.js
@@ -0,0 +1,12 @@
+const stationData = {
+  kxll: {
+    stationName: "KXLL",
+    location: "Juneau, AK 🇺🇸",
+    webUrl: "https://www.ktoo.org/kxll/",
+    streamUrl: "https://14613.live.streamtheworld.com/KXLLFMAAC.aac?dist=scrobbleradio",
+    apiUrl: "https://widgets.spinitron.com/widget/now-playing-v2?callback=_spinitron05891577247031242171657762111&station=kxll&num=0&sharing=0&player=0&cover=0&merch=0&meta=0",
+    spinPath: true,
+    timezone: "America/Juneau",
+    filter: ["KXLL"],
+  }
+};
