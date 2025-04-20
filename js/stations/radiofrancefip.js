92717d19d307fbc8ae74f42eaad6660b3f87e8fa
jbwharris <james@slideaway.ca>
2025-04-20 16:59:41 -0400

    Add RadioFrance FIP

---
+++ js/stations/radiofrancefip.js
@@ -0,0 +1,13 @@
+const stationData = {
+  radiofrancefip: {
+    stationName: "Radio France FIP",
+    artist: "now.secondLine.title",
+    song: "now.firstLine.title",
+    location: "Paris, France 🇫🇷",
+    webUrl: "https://www.nova.fr/",
+    streamUrl: "https://icecast.radiofrance.fr/fipelectro-hifi.aac",
+    apiUrl: "https://www.radiofrance.fr/fip/api/live?webradio=fip_electro",
+    timestamp: "now.startTime",
+    timezone: "Europe/Paris",
+  }
+};
