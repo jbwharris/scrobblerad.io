63cc0244ab747f2433985da0e74ab0dd3c164428
jbwharris <james@slideaway.ca>
2025-04-20 16:59:57 -0400

    Convert WYMS to Spinitron

--- js/stations/wyms.js
+++ js/stations/wyms.js
@@ -1,15 +1,12 @@
 const stationData = {
   wyms: {
-    artist: "data.0.artist",
-    song: "data.0.title",
-    album: "data.0.album",
     stationName: "88Nine",
     location: "Milwaukee, WI 🇺🇸",
     webUrl: "https://www.radiomilwaukee.org/",
     streamUrl: "https://wyms.streamguys1.com/live?platform=88nine",
-    apiUrl: "https://ca.api.iheart.com/api/v3/live-meta/stream/8380/trackHistory?limit=1",
-    needPath: true,
-    timestamp: "data.0.endTime",
+    apiUrl:
+      "https://widgets.spinitron.com/widget/now-playing-v2?callback=_spinitron206170750999458&station=wyms&num=0&sharing=0&player=0&cover=1&merch=0&meta=0",
+    spinPath: true,
     timezone: "America/Chicago",
   }
 };
