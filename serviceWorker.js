73c0e2fce9b8d251063ea67cbac05d005d25e3c0
jbwharris <james@slideaway.ca>
2025-04-20 17:02:38 -0400

    Iterate version

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const SCROBBLERADIO_CACHE = "app-v3.18";  // Updated cache version
+const SCROBBLERADIO_CACHE = "app-v3.5";  // Updated cache version
 const staticPlayer = SCROBBLERADIO_CACHE;
 const assets = [
   "/",
