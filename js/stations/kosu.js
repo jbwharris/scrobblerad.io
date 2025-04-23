e948bd7a51b3420d7ec25c032b8d731bb7d4db7c
jbwharris <james@slideaway.ca>
2025-04-22 21:28:55 -0400

    Change KOSU api source

--- js/stations/kosu.js
+++ js/stations/kosu.js
@@ -1,14 +1,11 @@
 const stationData = {
   kosu: {
     stationName: "KOSU - The Spy",
-    artist: "data.0.artist",
-    song: "data.0.title",
-    album: "data.0.album",
     location: "Oklahoma City, OK 🇺🇸",
     webUrl: "https://thespyfm.com/",
     streamUrl: "https://22063.live.streamtheworld.com/KOSUHD2.mp3?dist=scrobbleradio",
-    apiUrl: "https://ca.api.iheart.com/api/v3/live-meta/stream/7336/trackHistory?limit=1",
-    timestamp: "data.0.endTime",
+    apiUrl: "https://scraper2.onlineradiobox.com/us.kosu3?l=0",
+    orbPath: true,
     timezone: "America/Chicago",
     filter: ["KOSU", "The Spy"],
   }
