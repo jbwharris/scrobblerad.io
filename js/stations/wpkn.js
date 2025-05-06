b220e6597d8180b3cb68690b0082e698c5937f59
jbwharris <james@slideaway.ca>
2025-05-05 22:45:37 -0400

    Update feed URL

--- js/stations/wpkn.js
+++ js/stations/wpkn.js
@@ -3,7 +3,7 @@ const stationData = {
     stationName: "WPKN",
     location: "Bridgeport, CT 🇺🇸",
     webUrl: "https://wpkn.org/",
-    streamUrl: "http://live.adsciconsolidated.com/wpknhigh.mp3",
+    streamUrl: "https://ice25.securenetsystems.net/WPKN",
     apiUrl: "https://widgets.spinitron.com/widget/now-playing-v2?callback=_spinitron206170750999458&station=wpkn&num=0&sharing=0&player=0&cover=1&merch=0&meta=0",
     spinPath: true,
     timezone: "America/New_York",
