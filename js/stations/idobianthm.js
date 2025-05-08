f2f8731a223e19c77eb0bc80464364e715c93953
jbwharris <james@slideaway.ca>
2025-05-08 19:19:04 -0400

    Station up

--- js/stations/idobianthm.js
+++ js/stations/idobianthm.js
@@ -3,7 +3,7 @@ const stationData = {
     stationName: "idobi anthm",
     location: "Washington, DC 🇺🇸",
     webUrl: "https://idobi.com/",
-    streamUrl: "https://idobianthm.idobi.com/",
+    streamUrl: "https://anthm.idobistation.com/?aw_0_site.page=https%3A%2F%2Fscrobblerad.io%2F&",
     apiUrl: "https://scraper2.onlineradiobox.com/us.idobianthm?l=0",
     timestamp: "updated",
     timezone: "America/New_York",
--- js/stations/wext.js
+++ js/stations/wext.js
@@ -1,7 +1,7 @@
 const stationData = {
   wext: {
     artist: "onNow.song.artistName",
-    song: "onNow.song.trackName",  
+    song: "onNow.song.trackName",
     album: "onNow.song.collectionName",
     artist2: "tracklist.results.0.song.artistName",
     song2: "tracklist.results.0.song.trackName",
@@ -9,12 +9,12 @@ const stationData = {
     stationName: "WEXT",
     location: "Troy, NY 🇺🇸",
     webUrl: "https://wext.drupal.publicbroadcasting.net/  ",
-    streamUrl: "https://wmht.streamguys1.com/wext1?uuid=d4918beel",
+    streamUrl: "https://wmht.streamguys1.com/wext1",
     apiUrl:
       "https://api.composer.nprstations.org/v1/widget/5182cfc4e1c891fe553a5b52/tracks?format=json&limit=2&hide_amazon=false&hide_itunes=false&hide_arkiv=false&share_format=false",
     timestamp: "onNow.song._end_time",
     timestamp2: "tracklist.results.0.song._end_time",
     altPath: true,
     timezone: "America/New_York",
-  }
-};
+  },
+}
