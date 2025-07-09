00d2157dc7cecdef7e41378560f367dd4bf4e3a0
jbwharris <james@slideaway.ca>
2025-07-09 15:40:07 -0400

    KJFC

--- js/stations/kfjc.js
+++ js/stations/kfjc.js
@@ -6,7 +6,7 @@ const stationData = {
     album: "playlist.0.album_title",
     location: "Los Altos Hills, CA 🇺🇸",
     webUrl: "https://kfjc.org",
-    streamUrl: "http://netcast.kfjc.org/kfjc-320k-aac",
+    streamUrl: "https://netcast.kfjc.org/kfjc-320k-aac", 
     apiUrl: "https://kfjc.org/api/playlists/index.php",
     timestamp: "playlist.0.time_played",
     timezone: "UTC",
