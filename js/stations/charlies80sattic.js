d26b9fac9e9a69cf874398d83065f1a76f548e84
jbwharris <james@slideaway.ca>
2025-08-22 22:12:22 -0400

    Add Charlie's 80s Attic

---
+++ js/stations/charlies80sattic.js
@@ -0,0 +1,15 @@
+const stationData = {
+  charlies80sattic: {
+    stationName: "Charlie's 80s Attic",
+    artist: "artist",
+    song: "title",
+    album: "album",
+    location: "Blairville, GA 🇺🇸",
+    webUrl: "https://charlies80sattic.com",
+    streamUrl: "https://ice3.securenetsystems.net/CHARLIE",
+    apiUrl: "https://ca.api.iheart.com/api/v3/live-meta/stream/8538/currentTrackMeta",
+    duration: "trackDuration",
+    timestamp: "startTime",
+    timezone: "America/New_York",
+  }
+};
