5779e3a869e048545c3c7f3dc2332887042a4484
jbwharris <james@slideaway.ca>
2025-08-29 23:07:38 -0400

    Add 89X and 891. MAX FM

---
+++ js/stations/cimx.js
@@ -0,0 +1,15 @@
+const stationData = {
+  cimx: {
+    stationName: "89X - CIMX",
+    artist: "artist",
+    song: "title",
+    album: "album",
+    location: "Windsor, Ontario 🇨🇦",
+    webUrl: "https://89xradio.com/",
+    streamUrl: "https://15693.live.streamtheworld.com/CIMXFM_ADP/HLS/playlist.m3u8",
+    apiUrl: "https://ca.api.iheart.com/api/v3/live-meta/stream/8800/currentTrackMeta",
+    duration: "trackDuration",
+    timestamp: "startTime",
+    timezone: "America/New_York",
+  }
+};
---
+++ js/stations/ciso.js
@@ -0,0 +1,12 @@
+const stationData = {
+  ciso: {
+    stationName: "89.1 MAX FM - CISO",
+    location: "Orillia, ON 🇨🇦",
+    webUrl: "https://891maxfm.ca",
+    streamUrl: "https://bayshore.leanstream.co/CISOFM?",
+    orbPath: "ca.sunshine",
+    timezone: "America/Toronto",
+    updated: "last_played",
+    flipMeta: true,
+  }
+};
