80822ad66c27ab67b1a4bcb730721604914ecf33
jbwharris <james@slideaway.ca>
2025-10-15 11:54:02 -0400

    Update SoCal Sound and Sonic Coast

--- js/stations/socalsound.js
+++ js/stations/socalsound.js
@@ -6,11 +6,10 @@ const stationData = {
     album: "data.0.album",
     location: "Los Angeles, CA 🇺🇸",
     webUrl: "https://thesocalsound.org/",
-    streamUrl: "https://stream2.885fm.org/1",
+    streamUrl: "https://www.streamvortex.com:8444/s/12200",
     apiUrl: "https://ca.api.iheart.com/api/v3/live-meta/stream/5523/trackHistory?limit=1",
     filter: ["The SoCal Sound"],
     timestamp: "data.0.endTime",
     timezone: "America/Los_Angeles",
-    needPath: true,
   }
 };
--- js/stations/soniccoast.js
+++ js/stations/soniccoast.js
@@ -3,7 +3,7 @@ const stationData = {
     stationName: "The Sonic Coast",
     location: "Detroit, MI 🇺🇸",
     webUrl: "https://thesoniccoast.com/",
-    streamUrl: "https://das-edge13-live365-dal02.cdnstream.com/a85042_2",
+    streamUrl: "https://das-edge17-live365-dal02.cdnstream.com/a85042?_=823680",
     orbPath: "us.thesoniccoast",
     timestamp: "updated",
     timezone: "America/New_York",
