6effa4fa491219f8310bbe01c3463212b4995f44
jbwharris <james@slideaway.ca>
2025-11-25 09:59:54 -0500

    Responsive fixes

--- js/stations/kcrw.js
+++ js/stations/kcrw.js
@@ -4,7 +4,7 @@ const stationData = {
     song: "title",
     album: "album",
     stationName: "KCRW",
-    webUrl: "https://www.kcrw.com/shows/eclectic24/latest",
+    webUrl: "https://www.kcrw.com/",
     location: "Los Angeles, CA 🇺🇸",
     streamUrl: "https://streams.kcrw.com/kcrw_mp3",
     apiUrl: "https://tracklist-api.kcrw.com/Simulcast",
