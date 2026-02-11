6bc237613fdd09dc82aee3645be81822753908e1
jbwharris <james@slideaway.ca>
2026-02-10 22:16:28 -0500

    Update stream for CKUW

--- js/stations/ckuw.js
+++ js/stations/ckuw.js
@@ -3,11 +3,11 @@ const stationData = {
     stationName: "CKUW",
     location: "Winnipeg, MB 🇨🇦",
     webUrl: "https://www.ckuw.ca/",
-    streamUrl: "http://shout.mtl.gameservers.com:9025/;",
+    streamUrl: "https://streams.radiomast.io/fabaf1d8-1c26-4949-930f-2ce9874f5d45",
     spinPath: "ckuw",
     timezone: "America/Winnipeg",
     filter: ["CKUW"],
     quietStream: true,
-    gainBoost: 3
+    gainBoost: 3,
   }
 };
