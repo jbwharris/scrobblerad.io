376b8de63f5a1fdd652d1da3a8ee740d3bd8833e
jbwharris <james@slideaway.ca>
2026-03-11 22:03:50 -0400

    Fip fix

--- js/stations/radiofrancefip.js
+++ js/stations/radiofrancefip.js
@@ -11,19 +11,16 @@ const stationData = {
     proxyApi: true,
     filter: ["Club Jazzaflip", "Radio France", "electro", "Le Direct"],
     electro: {
-      stationName: "Radio France Fip: Electro",
       webUrl: "https://www.nova.fr/",
       streamUrl: "https://icecast.radiofrance.fr/fipelectro-hifi.aac",
       apiUrl: "https://www.radiofrance.fr/fip/api/live?webradio=fip_electro",
     },
     jazz: {
-      stationName: "Radio France Fip: Jazz",
       webUrl: "https://www.nova.fr/",
       streamUrl: "https://icecast.radiofrance.fr/fipjazz-hifi.aac",
       apiUrl: "https://www.radiofrance.fr/fip/api/live?webradio=fip_jazz",
     },
     rock: {
-      stationName: "Radio France Fip: Rock",
       webUrl: "https://www.nova.fr/",
       streamUrl: "https://icecast.radiofrance.fr/fiprock-hifi.aac",
       apiUrl: "https://www.radiofrance.fr/fip/api/live?webradio=fip_rock",
