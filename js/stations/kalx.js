ab74115d8c2db5ef39df2378ed8a2849f39f9a08
jbwharris <james@slideaway.ca>
2026-07-07 07:38:46 -0400

    Update KALX stream

--- js/stations/kalx.js
+++ js/stations/kalx.js
@@ -1,12 +1,10 @@
 const stationData = {
   kalx: {
-
     location: "Berkeley, CA 🇺🇸",
     webUrl: "https://kalx.berkeley.edu/",
-    streamUrl: "https://stream.kalx.berkeley.edu:8443/kalx-320.aac",
+    streamUrl: "https://ark3.spinitron.com/ark2/KALX-20260707T080000Z/index.m3u8",
     spinPath: "kalx",
     timezone: "America/Los_Angeles",
     filter: ["KALX"],
-    proxyStream: true,
   }
 };
