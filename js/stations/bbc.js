0dd2004e5692b6a967292deb287560a1ad95c976
jbwharris <james@slideaway.ca>
2026-08-19 20:39:30 -0400

    BBC update

--- js/stations/bbc.js
+++ js/stations/bbc.js
@@ -22,7 +22,7 @@ const stationData = {
       artist: "data.0.titles.primary",
       song: "data.0.titles.secondary",
       webUrl: "https://www.bbc.co.uk/sounds/play/live:bbc_6music",
-      streamUrl: "https://a.files.bbci.co.uk/ms6/live/3441A116-B12E-4D2F-ACA8-C1984642FA4B/audio/simulcast/hls/nonuk/audio_syndication_low_sbr_v1/cfs/bbc_6music.m3u8",
+      streamUrl: "https://a.files.bbci.co.uk/ms6/live/3441A116-B12E-4D2F-ACA8-C1984642FA4B/audio/simulcast/hls/uk/mobile_wifi_main_hd_abr_v2/aks/bbc_radio_six_indie_forever.m3u8",
       apiUrl: "https://rms.api.bbc.co.uk/v2/services/bbc_radio_six_indie_forever/segments/latest?experience=domestic&offset=0&limit=4",
       updated: "data.0.offset.start",
       proxyApi: true,
